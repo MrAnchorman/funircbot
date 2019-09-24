39e23f3573e12f1c9aa60580ffde6bf5ed25eb3f
MrAnchorman <mranchorman@bashbox.de>
2019-09-24 13:04:36 +0200

    Configration file writing

--- funircbot.py
+++ funircbot.py
@@ -7,6 +7,8 @@ import queue
 import os
 import argparse
 import logging
+import configparser
+import socket
 
 
 parser = argparse.ArgumentParser()
@@ -29,26 +31,92 @@ style = '{',
 format = '{asctime} [{levelname:7}] {message}',
 datefmt = '%d.%m.%Y %H:%M:%S')
 
+class Config:
+    def __init__(self, path):
+        self.path = path
+
+    def checkPath(self):
+        if os.path.isdir(self.path):
+            logging.debug('Config file is a path, not a file. Will create file named config.dat in given path')
+            self.path = os.path.join(self.path, 'config.dat')
+
+        if not os.access(self.path, os.R_OK):
+            logging.error('Given config file: {} cannot be read.'.format(self.path))
+            self.path = os.path.join('.', 'config.dat')
+
+        if not os.access(self.path, os.W_OK):
+            logging.error('Config file can not be written.')
+            self.path = os.path.join('.', 'config.dat')
+
+    def createConfig(self):
+        config = configparser.ConfigParser()
+        self.path = 'test.dat'
+        if not os.path.exists(self.path):
+            open(self.path, 'a').close()
+            
+
+        if os.path.getsize(self.path) < 10000:
+            logging.warning('Config file is too small. We will set it up')
+            
+            with open(self.path, 'w') as f:
+                config.add_section('IRCSERVER')
+                host = self.getServerAddress()
+                if host == False:
+                    raise Exception('Aborted by user')
+                else:
+                    config.set('IRCSERVER', 'server', host)
+
+                port = input('Port (SSL is usually 6697: ')
+                config.set('IRCSERVER', 'port', port)
+                
+                config.add_section('IRCUSER')
+                config.set('IRCUSER', 'nick', input('Bot\'s nickname: '))
+                config.set('IRCUSER', 'password', input('Bot\'s password (Warning: stored in clear text in config file!): '))
+                
+                config.add_section('IRCADMIN')
+                adminlist = list()
+                while True:
+                    adminnick = input('Nickname of administrator, c to cancel: ')
+                    if adminnick == 'c':
+                        if len(adminlist) < 1:
+                            print('You have to insert at least one administrator')
+                            continue
+                        else:
+                            break
+                    if adminnick != '':
+                        if adminnick in adminlist:
+                            print('You don\'t need to insert that admin twice')
+                        else:
+                            adminlist.append(adminnick)
+                config.set('IRCADMIN', 'administrators', ';'.join(adminlist))
+                
+                config.write(f)
+
+    def getServerAddress(self):
+        while True:
+            try:
+                host = input('Hostname for IRC Server: ')
+                if host == 'c':
+                    return False
+                socket.gethostbyname(host)
+                return host
+            except:
+                continue
+                
 
 # if there isn't any configuration file in the arguments list, give a default path
 if not args.configfile:
-    logging.debug('No logfile given per argument. Will use path {}.'.format(os.path.join('.', 'config.dat')))
-    configfile = os.path.join('.', 'config.dat')
+    logging.debug('No logfile given per argument. Will use path {}.'.format(os.path.join(os.getcwd(), 'config.dat')))
+    configfile = os.path.join(os.getcwd(), 'config.dat')
 
-if os.path.isdir(configfile):
-    logging.debug('Config file is a path, not a file. Will create file named config.dat in given path')
-    configfile = os.path.join(configfile, 'config.dat')
 
-if not os.access(configfile, os.R_OK):
-    logging.error('Given config file: {} cannot be read.'.format(configfile))
-    configfile = os.path.join('.', 'config.dat')
 
-if not os.access(configfile, os.W_OK):
-    logging.error('Config file can not be written.')
-    configfile = os.path.join('.', 'config.dat')
 
+    
 
 def main():
+    c = Config(configfile)
+    c.createConfig()
     plugins = dict()
     plugins['irc'] = importlib.import_module('IRC')
     irc = plugins['irc'].IRC()
